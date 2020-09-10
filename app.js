b37214740257d6a5e17a21fd25d849b4e224458a
huynhpham.pix <huynhpham.pix@gmail.com>
2020-09-10 22:31:58 +0700

    full code

--- app.js
+++ app.js
@@ -1,103 +1,30 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var bcrypt = require('bcryptjs');
+var logger = require('morgan');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var admin = require('./routes/admin');
-var cate = require('./routes/cate');
-var product = require('./routes/product');
-var cart = require('./routes/cart');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var salt = bcrypt.genSaltSync(10);
-var hash = bcrypt.hashSync("B4c0/\/", salt);
-
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
 var app = express();
 
-//var upload = multer({ dest: '/public/uploads/' })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
-mongoose.connect('mongodb://localhost:27017/webnodejs', { useMongoClient: true }, function (err, db) {
-  //neu ket noi khong thanh cong thi in ra loi
-  if (err) throw err;
-  //neu thanh cong thi log ra thong bao
-  console.log('Ket noi thanh cong');
-});
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(session({ secret: 'chauminhthien', resave: true, saveUninitialized: true }))
 
-
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-app.use(session({
-  secret: 'chauminhthien',
-  resave: true,
-  key: 'user',
-  saveUninitialized: true
-
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(flash());
-app.use(function(req, res, next){
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-	next();
-});
-
-
-
-
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/admin', admin);
-app.use('/admin/cate', cate);
-app.use('/admin/product', product);
-app.use('/admin/cart', cart);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
